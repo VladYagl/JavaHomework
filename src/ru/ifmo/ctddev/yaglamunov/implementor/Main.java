79e56fe89dab2206b919b979338b98146b66587d
Vlad <vlad082006@yandex.ru>
2017-02-25 23:48:15 +0300

    PogChamp I've done it.

---
+++ src/ru/ifmo/ctddev/yaglamunov/implementor/Main.java
@@ -0,0 +1,20 @@
+package ru.ifmo.ctddev.yaglamunov.implementor;
+
+import info.kgeorgiy.java.advanced.implementor.ImplerException;
+
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            new Implementor().implement(javax.imageio.IIOException.class, Paths.get("implementor"));
+            new Implementor().implement(javax.management.ImmutableDescriptor.class, Paths.get("implementor"));
+            new Implementor().implement(javax.naming.ldap.LdapReferralException.class, Paths.get("implementor"));
+            new Implementor().implement(javax.annotation.Generated.class, Paths.get("implementor"));
+            new Implementor().implement(javax.sql.rowset.CachedRowSet.class, Paths.get("implementor"));
+            new Implementor().implement(void.class, Paths.get("implementor"));
+        } catch (ImplerException e) {
+            e.printStackTrace();
+        }
+    }
+}
